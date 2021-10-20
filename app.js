26992d8512121e6448e82e746b1a8378ae6a672c
Amit Bhalla <amit9815@gmail.com>
2021-10-20 20:33:37 +0530

    Static files serving added

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const app = express();
 
 // Middleware
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(path.resolve(), 'public')));
 
 // Routes
 app.use('/admin', adminRoutes);
